c2dd4a6c17394dc92dc9227a5abf46ded5e0a63a
timinigi <timinigi@gmail.com>
2020-12-18 16:14:43 +0100

    Responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,9 @@ import React, { FC } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link,
   Switch,
 } from 'react-router-dom';
-
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import Toolbar from '@material-ui/core/Toolbar';
-
-import { makeStyles } from '@material-ui/core/styles';
 
 import Domu from './pages/Domu';
 import OMne from './pages/OMne';
@@ -20,54 +13,15 @@ import Kniha from './pages/Kniha';
 import Nenalezeno from './pages/Nenalezeno';
 
 import './App.css';
-
-const useStyles = makeStyles(theme => ({
-  toolbar: {
-    display: 'flex',
-    justifyContent: 'space-between'
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  link: {
-    textDecoration: 'none'
-  },
-}));
+import AppHeader from "./components/AppHeader";
 
 const App: FC = () => {
-  const classes = useStyles();
-
   return (
       <Router>
-        <AppBar color="primary" position="static" variant="outlined">
-          <Toolbar className={classes.toolbar}>
-            <div>
-              <Link className={classes.link} to="/">
-                <Button className={classes.menuButton}>
-                  Úvodní stránka
-                </Button>
-              </Link>
-              <Link className={classes.link} to="/o_mne">
-                <Button className={classes.menuButton}>
-                  O mně
-                </Button>
-              </Link>
-              <Link className={classes.link} to="/galerie">
-                <Button className={classes.menuButton}>
-                  Fotogalerie
-                </Button>
-              </Link>
-              <Link className={classes.link} to="/kniha">
-                <Button className={classes.menuButton}>
-                  Návštěvní kniha
-                </Button>
-              </Link>
-            </div>
-          </Toolbar>
-        </AppBar>
+        <AppHeader/>
 
         <main className="App">
-          <Container maxWidth="sm">
+          <Container maxWidth="lg">
             <Switch>
               <Route path="/" exact component={Domu} />
               <Route path="/o_mne" exact component={OMne} />
