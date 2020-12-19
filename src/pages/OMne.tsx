34095a2679365cf6c81b6cb5f813279ede394e64
Michal Planička <500376@mail.muni.cz>
2020-12-19 19:52:26 +0100

    O mně dokončeno

--- src/pages/OMne.tsx
+++ src/pages/OMne.tsx
@@ -11,6 +11,29 @@ const OMne: FC = () => {
           O mně
         </Typography>
       </Grid>
+        <Grid item xs={12}>
+            <Typography variant="body1" gutterBottom>
+                <img src={process.env.PUBLIC_URL + '/photos/o_mne.jpg'} alt='O mně' />
+            </Typography>
+        </Grid>
+        <Grid item xs={12}>
+            <Typography variant="body1" gutterBottom align="justify">
+                <em>&quot;Mám rád lidi, kteří někdy umějí vyjet mimo ze svých denních kolejí.&quot;</em>&nbsp;—&nbsp;F. R. Čech<br /><br />
+                Ano, mám rád lidi, jsem nadšený fotograf a koleje jsou můj denní chléb. Filozofie mého nadšení
+                spočívá v tom, že stejně jako fotograf v ateliéru aranžuje své modely, tak já aranžuji hrdiny
+                svých fotek v prostředí, ve kterém se pohybují. Baví mne přesvědčit je, aby &quot;vyjeli ze
+                svých kolejí&quot; a přijali za svou kompozici, kterou jsem jim ušil na míru.<br />
+                Budu rád, pokud se vám moje záběry budou líbit a těším se, že možná právě vy se mnou příště
+                vyjedete mimo své koleje.<br />
+                Fotky, vznikající pomocí výtečné techniky NIKON a SIGMA - ART, se zrodily v hlavě Blížence
+                skvělého ročníku 65.
+            </Typography>
+        </Grid>
+        <Grid item xs={12}>
+            <Typography variant="body1" gutterBottom align="right">
+                Milan
+            </Typography>
+        </Grid>
     </Grid>
   );
 };
